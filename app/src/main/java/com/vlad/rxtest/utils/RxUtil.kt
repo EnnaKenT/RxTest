dfb68c53263ffb50fd80ca8d94c6579de1fdddb7
Vlad <vladyslav.berdnikov@chisw.com>
2018-09-26 11:57:19 +0300

    rxUtil

--- app/src/main/java/com/vlad/rxtest/utils/RxUtil.kt
+++ app/src/main/java/com/vlad/rxtest/utils/RxUtil.kt
@@ -1,7 +1,6 @@
 package com.vlad.rxtest.utils
 
 import android.util.Log
-import android.view.View
 import io.reactivex.Maybe
 import io.reactivex.Observable
 import io.reactivex.Single
@@ -9,6 +8,16 @@ import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 
 fun <T> Single<T>.logThread(): Single<T> = map {
+    Log.d("duck", "threadName ${Thread.currentThread()}")
+    it
+}
+
+fun <T> Observable<T>.logThread(): Observable<T> = map {
+    Log.d("duck", "threadName ${Thread.currentThread()}")
+    it
+}
+
+fun <T> Maybe<T>.logThread(): Maybe<T> = map {
     Log.d("duck", "threadName ${Thread.currentThread()}")
     it
 }
@@ -25,7 +34,3 @@ fun <T> Observable<T>.customSchedulers(): Observable<T> =
 fun <T> Maybe<T>.customSchedulers(): Maybe<T> =
         subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-
-fun View.setVisible() {
-    visibility = View.VISIBLE
-}
